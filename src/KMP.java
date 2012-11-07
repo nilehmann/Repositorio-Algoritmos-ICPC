84f350ec025d476ad6a5d1d64b2db6e9d47b6a7b
Felipe Contreras <felipe.contreras.s@gmail.com>
2012-11-07 20:51:03 -0300

    Agregado KMP

--- src/KMP.java
+++ src/KMP.java
@@ -3,29 +3,26 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-
 public class KMP {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		String s;
-		while(!(s = in.readLine()).equals("Si No")){
+		while (!(s = in.readLine()).equals("Si No")) {
 			StringTokenizer st = new StringTokenizer(s);
-			System.out.println(kmpSearch(st.nextToken().toCharArray(), st.nextToken().toCharArray(), 0));
+			System.out.println(kmpSearch(st.nextToken().toCharArray(), st
+					.nextToken().toCharArray(), 0));
 		}
 	}
-	
-	public static int kmpSearch(char[] pattern, char[] text, int start)
-	{
+
+	public static int kmpSearch(char[] pattern, char[] text, int start) {
 		int[] f = new int[pattern.length];
 		f[0] = 0;
-		for (int j = 1; j < f.length; j++)
-		{
+		for (int j = 1; j < f.length; j++) {
 			int i = f[j - 1];
 			while (i > 0 && pattern[i] != pattern[j])
-			{
 				i = f[i];
-			}
+			
 			if (pattern[i] == pattern[j])
 				f[j] = i;
 			else
@@ -34,16 +31,13 @@ public class KMP {
 
 		int k = start;
 		int j = 0;
-		while (k < text.length && j < pattern.length)
-		{
+		while (k < text.length && j < pattern.length) {
 			while (j > 0 && text[k] != pattern[j])
-			{
 				j = f[j];
-			}
+			
 			if (text[k] == pattern[j])
-			{
 				j++;
-			}
+			
 			k++;
 		}
 		if (j == pattern.length)
