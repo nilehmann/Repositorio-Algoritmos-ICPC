a04b0e32afd0c9e5172bc1d7a5df9e94ca6dd7f2
Rodolfo Gutiérrez <rewindthetrust@hotmail.com>
2012-11-08 23:03:22 -0300

    Ordené el código

--- src/Kruskal.java
+++ src/Kruskal.java
@@ -2,69 +2,69 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
-
 public class Kruskal {
 	static int VMAX = 100000;
 	static int EMAX = 100000;
-	
+
 	static int E;
 	static int V;
 	static Edge edges[] = new Edge[EMAX];
 
 	static int parent[] = new int[EMAX];
 	static int rank[] = new int[EMAX];
-	
-	static List<Edge> kruskal(){
+
+	static List<Edge> kruskal() {
 		init();
-		Arrays.sort(edges, 0 , E);
+		Arrays.sort(edges, 0, E);
 		List<Edge> MST = new LinkedList<Edge>();
-		for(int i = 0; i < E; ++i){
+		for (int i = 0; i < E; ++i) {
 			Edge e = edges[i];
-			if(find(e.startNode) != find(e.endNode)){
+			if (find(e.startNode) != find(e.endNode)) {
 				union(e.startNode, e.endNode);
 				MST.add(e);
 			}
 		}
 		return MST;
 	}
-	
-	static void init(){
-		for(int i = 0; i < V; ++i)
+
+	static void init() {
+		for (int i = 0; i < V; ++i)
 			parent[i] = i;
 	}
-	
-	static int find(int x){
-		if(x != parent[x])
+
+	static int find(int x) {
+		if (x != parent[x])
 			parent[x] = find(parent[x]);
 		return parent[x];
 	}
-	
-	static void union(int x, int y){
-		if(rank[x] < rank[y]){
+
+	static void union(int x, int y) {
+		if (rank[x] < rank[y]) {
 			int temp = y;
 			y = x;
 			x = temp;
 		}
 		parent[y] = x;
-		rank[x] = rank[x] + rank[y] + 1;			
+		rank[x] = rank[x] + rank[y] + 1;
 	}
-	
+
 }
 
-class Edge implements Comparable<Edge>{
+class Edge implements Comparable<Edge> {
 	int startNode;
 	int endNode;
 	int dist;
-	public Edge(int sn, int en, int dist){
+
+	public Edge(int sn, int en, int dist) {
 		startNode = sn;
 		endNode = en;
 		this.dist = dist;
 	}
-	
+
 	public int compareTo(Edge o) {
-		if(dist < o.dist)
+		if (dist < o.dist)
 			return -1;
-		if(dist > o.dist)
+		if (dist > o.dist)
 			return 1;
 		return 0;
 	}
--- src/SuffixArray.java
+++ src/SuffixArray.java
@@ -8,129 +8,127 @@ import java.util.StringTokenizer;
 public class SuffixArray {
 	static int maxlg = 20;
 	static int maxlen = 100000;
-	
+
 	static String S;
 	static int N;
 	static entry sa[];
-	static int	P[][];
+	static int P[][];
 	static int stp;
 	static int lcp[];
-	
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
-		
+
 		P = new int[maxlg][maxlen];
 		sa = new entry[maxlen];
 		lcp = new int[maxlen];
-		
+
 		S = in.readLine();
 		while (S.charAt(0) != '*') {
 			N = S.length();
 			buildSA();
-			
-						
+
 			int c = 0;
 			int lcpant = 0;
 			int l;
-//			buildLCP();
+			// buildLCP();
 			for (int i = 1; i < N; i++) {
-//				l = lcp[i];
-				l = getlcp(sa[i].p, sa[i-1].p);
-				
-				if(l >= lcpant){
-					c += l -lcpant;
+				// l = lcp[i];
+				l = getlcp(sa[i].p, sa[i - 1].p);
+
+				if (l >= lcpant) {
+					c += l - lcpant;
 				}
 				lcpant = l;
 			}
 			System.out.println(c);
-			
 
-			S = in.readLine();		
+			S = in.readLine();
 		}
-		
+
 	}
-	
-	
-	static int getlcp ( int x, int y) {
+
+	static int getlcp(int x, int y) {
 		int k, ret = 0;
-		if (x == y) return N - x;
-		for (k = stp - 1; k >= 0 && x < N && y < N; k--)		
-			if (P[k][x] == P[k][y]){
-				x += 1 << k; 
-				y += 1 << k; 
+		if (x == y)
+			return N - x;
+		for (k = stp - 1; k >= 0 && x < N && y < N; k--)
+			if (P[k][x] == P[k][y]) {
+				x += 1 << k;
+				y += 1 << k;
 				ret += 1 << k;
 			}
 		return ret;
 	}
-	
-	
+
 	static void buildLCP() {
 		int h = 0, rank[] = new int[N];
-		for (int i = 0; i < N; i++) 
+		for (int i = 0; i < N; i++)
 			rank[sa[i].p] = i;
 		for (int i = 0; i < N; i++) {
-			if(rank[i] > 0){
-				int k = sa[rank[i] - 1].p; //El que viene antes del sufijo que parte en i;
-				while(i+h < N && k+h < N && S.charAt(i+h) == S.charAt(k+h)) h++;
+			if (rank[i] > 0) {
+				int k = sa[rank[i] - 1].p; // El que viene antes del sufijo que
+											// parte en i;
+				while (i + h < N && k + h < N
+						&& S.charAt(i + h) == S.charAt(k + h))
+					h++;
 				lcp[rank[i]] = h;
-				if(h > 0) h--;
+				if (h > 0)
+					h--;
 			}
 		}
 	}
-	
-	static void buildSA(){
+
+	static void buildSA() {
 		for (int i = 0; i < N; i++) {
-			P[0][i] = S.charAt(i);		
+			P[0][i] = S.charAt(i);
 		}
-		
+
 		int cnt;
 		boolean salir = false;
-		for(stp = 1, cnt = 1; !salir; cnt <<=1 , ++stp ){
+		for (stp = 1, cnt = 1; !salir; cnt <<= 1, ++stp) {
 			for (int i = 0; i < N; i++) {
-				int b = i+cnt < N ? P[stp-1][i+cnt] : -1;
-				sa[i] = new entry(P[stp-1][i], b, i);
+				int b = i + cnt < N ? P[stp - 1][i + cnt] : -1;
+				sa[i] = new entry(P[stp - 1][i], b, i);
 			}
-			Arrays.sort(sa,0, N);
-			
+			Arrays.sort(sa, 0, N);
+
 			salir = true;
 			for (int i = 0; i < N; i++) {
-				if(i > 0 && sa[i].equals(sa[i-1])){
+				if (i > 0 && sa[i].equals(sa[i - 1])) {
 					salir = false;
-					P[stp][sa[i].p] = P[stp][sa[i-1].p];
-				}
-				else
+					P[stp][sa[i].p] = P[stp][sa[i - 1].p];
+				} else
 					P[stp][sa[i].p] = i;
 			}
 		}
 	}
-	
+
 }
 
-class entry implements Comparable<entry>{
+class entry implements Comparable<entry> {
 	int p;
 	int first;
 	int second;
-	
-	public entry(int a, int b, int c){
+
+	public entry(int a, int b, int c) {
 		first = a;
 		second = b;
 		p = c;
 	}
-	
-	
-	public boolean equals(Object o){
+
+	public boolean equals(Object o) {
 		entry e = (entry) o;
 		return e.first == this.first && this.second == e.second;
 	}
-	
+
 	@Override
 	public int compareTo(entry o) {
-		if(this.first == o.first)
+		if (this.first == o.first)
 			return this.second - o.second;
 		return this.first - o.first;
 	}
-	
+
 }
